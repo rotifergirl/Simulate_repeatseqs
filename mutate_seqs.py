7f4b89ac3476aebb28ea6a4dbcb08013e0f76178
rotifergirl <jblommaert92@gmail.com>
2017-09-27 14:57:38 +0200

    Create mutate_seqs.py

---
+++ mutate_seqs.py
@@ -0,0 +1,26 @@
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from random import random,choice
+
+
+
+def read_file(file_name):
+	records=list(SeqIO.parse(file_name,"fastq"))
+
+mutated_reads=[]
+def mutate_reads(file_name,mutation_freq):
+	for read in records:
+		sequence=(list(records[read].seq))
+		for i in range(0,len(sequence)):
+			val=random()
+			if val < mutation_freq:
+				sequence[i]=choice([x for x in "ACTG"])
+			mut_seq=''.join(sequence)
+			record = SeqRecord(records[read].id,'version_%i'%(i))
+			record.seq=mut_seq
+		mutated_reads.append(record)
+    
+SeqIO.write(mutated_reads,'file_name_%i.fq'%(1),"fastq")
+
+
