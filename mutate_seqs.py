392c2e6d40c7b09fcfc616b18a0e39a2b4d222bf
rotifergirl <jblommaert92@gmail.com>
2017-09-28 13:55:34 +0200

    It works!

--- mutate_seqs.py
+++ mutate_seqs.py
@@ -6,23 +6,20 @@ import sys
 
 
 mutated_reads=[]
-def main(file_name,mutation_freq,out_file):
+def main(file_name,mutation_freq,out_file,iterations):
 	records=list(SeqIO.parse(file_name,"fastq"))
-	for read in records:
-		sequence=(list(read.seq))
-		for i in range(0,len(sequence)):
-			val=random()
-			if val < mutation_freq:
-				sequence[i]=choice([x for x in "ACTG"])
-		mut_seq=''.join(sequence)
-		record = SeqRecord(Seq(mut_seq),id=read.id,name=read.name,description=read.description)
-		record.letter_annotations["phred_quality"]=read.letter_annotations["phred_quality"]
-		mutated_reads.append(record)
+	for iter in range(0,iterations):
+		for read in records:
+			sequence=(list(read.seq))
+			for i in range(0,len(sequence)):
+				val=random()
+				if val < mutation_freq:
+					sequence[i]=choice([x for x in "ACTG"])
+			mut_seq=''.join(sequence)
+			record = SeqRecord(Seq(mut_seq),id=read.id,name=read.name,description=read.description)
+			record.letter_annotations["phred_quality"]=read.letter_annotations["phred_quality"]
+			mutated_reads.append(record)
 	SeqIO.write(mutated_reads,out_file,"fastq")
 
 if __name__ == "__main__":
-    main(sys.argv[1], float(sys.argv[2]),sys.argv[3])
-    
-
-
-
+    main(sys.argv[1], float(sys.argv[2]),sys.argv[3],int(sys.argv[4]))
